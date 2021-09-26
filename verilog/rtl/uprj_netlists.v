8d3aabedb59a22463d6624e196555caf898018c1
nofal <ahmad.nofal@efabless.com>
2021-09-26 23:31:24 +0200

    EFIC-176: support new directory structure in  --- make verify-io_ports ---  target
    EFIC-176: support new directory structure in  --- make verify-io_ports ---  target

--- verilog/rtl/uprj_netlists.v
+++ verilog/rtl/uprj_netlists.v
@@ -23,6 +23,6 @@
     `include "gl/user_project_wrapper.v"
     `include "gl/user_proj_example.v"
 `else
-    `include "user_project_wrapper.v"
-    `include "user_proj_example.v"
+    `include "rtl/user_project_wrapper.v"
+    `include "rtl/user_proj_example.v"
 `endif
